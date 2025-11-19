6d65392df7ca80ac1eca643dc87100467ae062b8
david <mail@nonswag.dev>
2025-11-19 20:59:24 +0100

    Add Gradle wrapper scripts and configs

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    id("java")
+    id("java-library")
+    id("maven-publish")
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
+    withSourcesJar()
+    withJavadocJar()
+}
+
+tasks.compileJava {
+    options.release.set(21)
+}
+
+group = "net.thenextlvl"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("org.jspecify:jspecify:1.0.0")
+    compileOnlyApi("net.kyori:adventure-text-logger-slf4j:4.26.0-SNAPSHOT")
+    compileOnlyApi("net.kyori:adventure-text-minimessage:4.26.0-SNAPSHOT")
+
+    testCompileOnly("org.jspecify:jspecify:1.0.0")
+    testImplementation("net.kyori:adventure-text-logger-slf4j:4.26.0-SNAPSHOT")
+    testImplementation("net.kyori:adventure-text-minimessage:4.26.0-SNAPSHOT")
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.slf4j:slf4j-simple:2.1.0-alpha1")
+    testImplementation(platform("org.junit:junit-bom:5.14.0"))
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+        showCauses = true
+        showExceptions = true
+    }
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+        pom.scm {
+            val repository = "TheNextLvl-net/i18n"
+            url.set("https://github.com/$repository")
+            connection.set("scm:git:git://github.com/$repository.git")
+            developerConnection.set("scm:git:ssh://github.com/$repository.git")
+        }
+    }
+    repositories.maven {
+        val channel = if ((version as String).contains("-pre")) "snapshots" else "releases"
+        url = uri("https://repo.thenextlvl.net/$channel")
+        credentials {
+            username = System.getenv("REPOSITORY_USER")
+            password = System.getenv("REPOSITORY_TOKEN")
+        }
+    }
+}
