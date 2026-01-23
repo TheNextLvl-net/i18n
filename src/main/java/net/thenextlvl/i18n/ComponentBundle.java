cae6a3b2bcf66b19701759dc409679097cdcc48c
david <mail@nonswag.dev>
2026-01-23 10:04:19 +0100

    Update Scope enum with NONE option

--- src/main/java/net/thenextlvl/i18n/ComponentBundle.java
+++ src/main/java/net/thenextlvl/i18n/ComponentBundle.java
@@ -444,7 +444,11 @@ public sealed interface ComponentBundle permits ComponentBundleImpl {
         /**
          * This scope will only fill in missing data
          */
-        FILL;
+        FILL,
+        /**
+         * This scope will not perform any validation
+         */
+        NONE;
 
         /**
          * Determines whether the current scope involves filtering unused data.
